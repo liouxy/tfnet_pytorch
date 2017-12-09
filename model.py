b21a7089a6ccaa708a6acd8b6a261791e4024c0e
xyliu <yblxy1993@gmail.com>
2017-12-09 17:51:53 +0800

    update the TFNet

--- model.py
+++ model.py
@@ -32,9 +32,9 @@ class _Residual_Block(nn.Module):
         return output
 
 
-class Net(nn.Module):
+class ResNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(ResNet, self).__init__()
         self.prelu = nn.PReLU()
         self.conv1_1_pan = nn.Conv2d(in_channels=1,
                                      out_channels=16,
@@ -136,9 +136,9 @@ class Net(nn.Module):
         return out
 
 
-class TFNet(nn.Module):
+class TFNet_old(nn.Module):
     def __init__(self):
-        super(TFNet, self).__init__()
+        super(TFNet_old, self).__init__()
         self.encoder1_pan=nn.Sequential(
             nn.Conv2d(in_channels=1,
                       out_channels=32,
@@ -276,7 +276,152 @@ class TFNet(nn.Module):
 
         return restore3
 
+class TFNet(nn.Module):
+    def __init__(self):
+        super(TFNet, self).__init__()
+        self.encoder1_pan=nn.Sequential(
+            nn.Conv2d(in_channels=1,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=32,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU()
+        )
+        self.encoder2_pan = nn.Sequential(
+            nn.Conv2d(in_channels=32,
+                      out_channels=32,
+                      kernel_size=2,
+                      stride=2),
+            nn.PReLU()
+        )
+        self.encoder1_lr=nn.Sequential(
+            nn.Conv2d(in_channels=4,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=32,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU())
+        self.encoder2_lr = nn.Sequential(
+            nn.Conv2d(in_channels=32,
+                      out_channels=32,
+                      kernel_size=2,
+                      stride=2),
+            nn.PReLU()
+        )
+        self.fusion1=nn.Sequential(
+            nn.Conv2d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU()
+        )
+        self.fusion2=nn.Sequential(
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=2,
+                      stride=2),
+            nn.PReLU(),
+        )
+        self.restore1=nn.Sequential(
+            nn.Conv2d(in_channels=128,
+                      out_channels=256,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1,
+                      ),
+            nn.PReLU(),
+            nn.ConvTranspose2d(in_channels=256,
+                               out_channels=128,
+                               kernel_size=2,
+                               stride=2),
+            nn.PReLU())
+        self.restore2=nn.Sequential(
+            nn.Conv2d(in_channels=256,
+                      out_channels=128,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.ConvTranspose2d(in_channels=128,
+                               out_channels=64,
+                               kernel_size=2,
+                               stride=2),
+            nn.PReLU()
+        )
+        self.restore3=nn.Sequential(
+            nn.Conv2d(in_channels=128,
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=64,
+                      out_channels=4,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.Tanh()
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
+    def forward(self, x_pan, x_lr):
+        encoder1_pan = self.encoder1_pan(x_pan)
+        encoder1_lr = self.encoder1_lr(x_lr)
 
+        encoder2_pan = self.encoder2_pan(encoder1_pan)
+        encoder2_lr = self.encoder2_lr(encoder1_lr)
 
+        fusion1 = self.fusion1(torch.cat((encoder2_pan, encoder2_lr), dim=1))
+        fusion2 = self.fusion2(fusion1)
 
+        restore1 = self.restore1(fusion2)
+        restore2 = self.restore2(torch.cat((restore1, fusion1),dim=1))
+        restore3 = self.restore3(torch.cat((restore2, encoder1_lr, encoder1_pan), dim=1))
+
+        return restore3
